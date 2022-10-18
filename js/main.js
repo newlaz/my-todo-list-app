50669df85d7d3726ccf576fb8796f03985975f8f
Bryan Lazo <lebry4n@gmail.com>
2022-10-18 13:18:53 -0600

    Creating check task function

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
-const templateTask = document.getElementById('templateTask').content;
-const inputTask = document.querySelector('.inputForm');
-const listContainer = document.querySelector('.cardsList');
-const btnCreate = document.querySelector('[data-form-btn]');
+// Protegiendo el codigo para que el usuario no puede acceder a el
 
-const createTask = () => {
-    templateTask.querySelector('.task').textContent = inputTask.value;
-    const templateClone = templateTask.cloneNode(true);
-    listContainer.appendChild(templateClone);
-}
+(() => {
 
-btnCreate.addEventListener('click', e => {
-    e.preventDefault(); // Previene que se recargue la pagina cuando se estan recibiendo datos de un formulario
-    if(inputTask.value !== ''){
-        createTask();
-    }else {
-        alert('¡Te faltó el nombre de la tarea!');
+    const templateTask = document.getElementById('templateTask').content;
+    const inputTask = document.querySelector('.inputForm');
+    const listContainer = document.querySelector('.cardsList');
+    const btnCreate = document.querySelector('[data-form-btn]');
+
+    const createTask = () => {
+        templateTask.querySelector('.task').textContent = inputTask.value;
+        const templateClone = templateTask.cloneNode(true);
+        listContainer.appendChild(templateClone);
     }
-    inputTask.value = '';
-});
+
+    const checkTask = (element) => {
+        // Si existe la clase la desactiva, si no, la activa
+        element.classList.toggle('far');
+        element.classList.toggle('fas');
+    }
+
+    
+
+    btnCreate.addEventListener('click', e => {
+        e.preventDefault(); // Previene que se recargue la pagina cuando se estan recibiendo datos de un formulario
+        if (inputTask.value !== '') {
+            createTask();
+        } else {
+            alert('¡Te faltó el nombre de la tarea!');
+        }
+        inputTask.value = '';
+    });
+
+    listContainer.addEventListener('click', (e) => {
+        if (e.target.classList.contains('fa-check-square')) {
+            checkTask(e.target);
+        } 
+        e.stopPropagation();
+    });
+
+})();
